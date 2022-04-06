d78426de6ccfd74dd867d81d4540c252aeb35b03
Alex Santos <amelo3433@gmail.com>
2022-04-05 23:25:32 -0300

    Endpoint /clientes/{id} Disponível

---
+++ src/main/java/com/alexsantos/sistemapedidos/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.alexsantos.sistemapedidos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexsantos.sistemapedidos.domain.Cliente;
+import com.alexsantos.sistemapedidos.repositories.ClienteRepository;
+import com.alexsantos.sistemapedidos.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id:" + id + " Tipo:" + Cliente.class.getName()));
+	}
+}
