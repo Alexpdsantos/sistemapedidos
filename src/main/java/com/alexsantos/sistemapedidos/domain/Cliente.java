a31cf6d1df3a9fcde89e01d6fbb8736f7d50859c
Alex Santos <amelo3433@gmail.com>
2022-04-13 23:55:37 -0300

    Endpoint Pedidos/{id} disponível

--- src/main/java/com/alexsantos/sistemapedidos/domain/Cliente.java
+++ src/main/java/com/alexsantos/sistemapedidos/domain/Cliente.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.alexsantos.sistemapedidos.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable{
@@ -28,7 +28,6 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -36,6 +35,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
