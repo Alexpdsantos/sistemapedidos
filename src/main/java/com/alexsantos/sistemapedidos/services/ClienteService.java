2da92a37bc7ea69e31509f907c8148e19ecaef65
Alex Santos <amelo3433@gmail.com>
2022-05-04 23:13:16 -0300

    Metodo POST para Clientes

--- src/main/java/com/alexsantos/sistemapedidos/services/ClienteService.java
+++ src/main/java/com/alexsantos/sistemapedidos/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alexsantos.sistemapedidos.domain.Cidade;
 import com.alexsantos.sistemapedidos.domain.Cliente;
+import com.alexsantos.sistemapedidos.domain.Endereco;
+import com.alexsantos.sistemapedidos.domain.enums.TipoCliente;
 import com.alexsantos.sistemapedidos.dto.ClienteDTO;
+import com.alexsantos.sistemapedidos.dto.ClienteNewDTO;
 import com.alexsantos.sistemapedidos.repositories.ClienteRepository;
+import com.alexsantos.sistemapedidos.repositories.EnderecoRepository;
 import com.alexsantos.sistemapedidos.services.exceptions.DataIntegrityException;
 import com.alexsantos.sistemapedidos.services.exceptions.ObjectNotFoundException;
 
@@ -22,12 +28,23 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id:" + id + " Tipo:" + Cliente.class.getName()));
 	}
-	
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -39,26 +56,44 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma Cliente que possui produtos!");
+			throw new DataIntegrityException("Não é possível excluir pois há entidades relacionadas!");
 		}
 	}
 
 	public List<Cliente> findAll() {
 		return repository.findAll();
 	}
-	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		PageRequest pageRequest	= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
 	}
-		
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cidade = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, null);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
+	}
+
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 }
